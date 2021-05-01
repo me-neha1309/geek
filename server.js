c5038e9ba843282e325aa63f2491b6d618682885
Aditya <adityaagupta31@gmail.com>
2021-05-01 20:22:08 +0530

    ejs bug fixed

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, 'views'));
-
-console.log(path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
 
 app.get('/', (req, res) => {
     res.send('Hello World...');
@@ -24,5 +24,5 @@ app.get('/user', (req, res) => {
 
 
 const server = app.listen(process.env.PORT || 3999, () => {
-    console.log(`Listening....`) 
+    console.log(`Listening....`)
 })
